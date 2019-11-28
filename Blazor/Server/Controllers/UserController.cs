a245cb03f1b67b260b131e0bd9c3101329c4399d
Katarina Milic <kaca_milic@hotmail.com>
2019-11-28 21:44:50 +0100

    Importing project

---
+++ Blazor/Server/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazor.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<User> GetUsers()
+        {
+            var output = new List<User>
+            {
+                new User
+                {   Username = "AP",
+                    FirstName = "Aleksa",
+                    LastName = "Pavlovic",
+                    Password = "aleksa"
+
+                },
+                new User
+                {   Username = "KM",
+                    FirstName = "Katarina",
+                    LastName = "Milic",
+                    Password = "katarina"
+
+                },
+                new User
+                {   Username = "VM",
+                    FirstName = "Vidan",
+                    LastName = "Milojevic",
+                    Password = "vidan"
+
+                },
+                new User
+                {
+                    Username = "LM",
+                    FirstName = "Lazar",
+                    LastName = "Miric",
+                    Password = "lazar"
+
+                }
+            };
+            return output;
+        }
+        [HttpPost]
+        public void Post (User user)
+        {
+                
+        }
+
+    }
+}
---
+++ Blazor/Shared/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Blazor.Shared
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
